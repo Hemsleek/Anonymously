af8ec25a3e594dea944b420b0970d040175dfb19
hemsleek <asiyanbimubashir@gmail.com>
2020-11-21 23:58:47 +0100

    fix:routing

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -3,12 +3,14 @@ import Default from '../layouts/Default'
 
 import './Messages.scss'
 
-const Messages = () => {
-
+const Messages = ({match}) => {
+ console.log(match)
     return( 
         <Default>
-            <form  onSubmit={(ev) => ev.preventDefault()} className="max-w-md bg-white text-indigo-900  h-auto rounded shadow-lg p-5 mx-auto mt-32 ">
+            <span className="text-center text-lg block mt-32 text-white"> Leave a message for {match.params.username} </span>
+            <form  onSubmit={(ev) => ev.preventDefault()} className="max-w-md bg-white text-indigo-900  h-auto rounded shadow-lg p-5 mx-auto ">
                 <div className="form-group mb-3">
+                   
                     <textarea className="outline-none bg-gray-200 p-2" name="message" id="" cols="30" rows="10"></textarea>
                 </div>
 
