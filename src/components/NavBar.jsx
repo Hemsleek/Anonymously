da8cb17f20c95d568a2dbd6798bcfdccfcd2c5a4
hemsleek <asiyanbimubashir@gmail.com>
2020-11-22 03:37:46 +0100

    feat: text indicator

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
     return (
         <nav className="NavBar flex h-12 px-2 font-bold items-center bg-indigo-400 text-white">
             <Link className="brand-logo font-extrabold text-xl" to='/'>Anonymously</Link>
-            <spacer className="flex-grow" />
+            <span className="spacer flex-grow" />
             {
                 Tabs.map(({route,text},routeIndex) => <Link onClick={() => setActiveTab(text)} to={route} className={`tab mr-5 hover:text-indigo-700 ${activeTab===text? 'selected' : ""}`} key={`navbar-route${routeIndex}`}>
                     {text}
